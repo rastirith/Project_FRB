209f5a58ef647d05da4a42a04c53a9d049a92baa
OskNum <44180119+OskNum@users.noreply.github.com>
2018-12-11 19:13:55 +0000

    Tuesday's changes, added optimisation file

--- machine_learning/Osk/ml_opt_loop.py
+++ machine_learning/Osk/ml_opt_loop.py
@@ -11,6 +11,7 @@ import scipy.cluster.hierarchy as hcluster
 import sys
 import warnings
 import itertools
+import random
 
 warnings.filterwarnings("ignore",category=mpl.cbook.mplDeprecation)
 warnings.filterwarnings("ignore",category=RuntimeWarning)
@@ -84,12 +85,29 @@ def DF(path):
     Tfile.close()
     return df
 
+step = 0.001
+
+iterations = 200
+iter_arr = np.zeros(200)
+score_struct = []
+TP_iter = np.zeros((int(1/step),iterations))
+FP_iter = np.zeros((int(1/step),iterations))
+TN_iter = np.zeros((int(1/step),iterations))
+FN_iter = np.zeros((int(1/step),iterations))
+
+for i in range(iterations):
+    a = random.uniform(0,3)
+    b = random.uniform(0,3)
+    c = random.uniform(0,3)
+    #d = random.uniform(0,3)
+    score_struct.append([a,b,c,c])
+    
 
 counter = 0
-true_pos = 0
-false_pos = 0
-true_neg = 0
-false_neg = 0
+true_pos = np.zeros(int(1/step))
+false_pos = np.zeros(int(1/step))
+true_neg = np.zeros(int(1/step))
+false_neg = np.zeros(int(1/step))
 
 pos_array = [
         1, 3, 7, 8, 10, 14, 19, 20, 22, 24, 30, 31, 38,
@@ -108,22 +126,10 @@ for file in glob.glob(os.getcwd() + '\idir\\' + "*.dat"):
     
 ratios = []
 
-conf_D = []
-conf_C = []
-conf_B = []
-conf_A = []
-
-
 for i in range(0,72): 
     print(i)
     para = 0
     path=i
-    
-    conf_A.append(0)
-    conf_B.append(0)
-    conf_C.append(0)
-    conf_D.append(0)
-    
     #setting which file to open
     FILE = source_paths[path]
     #getting df for test file
@@ -248,223 +254,163 @@ for i in range(0,72):
         else:
             counter += 1
             
-            weight_1 = 1/2
-            weight_2 = 3/4
-            weight_3 = -0.1
-            check_1 = 0.075
-            check_2 = 0.15
-            check_3 = 0.25
-            score = [1,1.5,1.25,1.25]
-            rating = 0
-        
-            sub_arr1 = meanDM[0:max_ind + 1]
-            sub_arr2 = meanDM[max_ind:len(meanDM)]
-            
-            diffs1 = [abs(e[1] - e[0]) for e in itertools.permutations(sub_arr1, 2)]
-            diffs2 = [abs(e[1] - e[0]) for e in itertools.permutations(sub_arr2, 2)]
-            
-            avg_diff1 = sum(diffs1)/len(diffs1)
-            avg_diff2 = sum(diffs2)/len(diffs2)
-            
-            diffDiff = abs(avg_diff1 - avg_diff2)
-            sumDiff = avg_diff1 + avg_diff2
-            diffRatio = diffDiff/sumDiff
-            
-            for i in range(max_ind - 1, -1, -1):
-                ratio=meanSN[i]/meanSN[i+1]
-            
-                if ((ratio>=(1-check_1)) and (ratio<=1)):
-                    rating += 0
-                elif ((ratio>=(1-check_2)) and (ratio<=1)):
-                    rating += weight_1*score[max_ind-(i+1)]
-                elif ((ratio>=(1-check_3)) and (ratio<=1)):
-                    rating += weight_2*score[max_ind-(i+1)]
-                elif (ratio<=1):
-                    rating += score[max_ind-(i+1)]
-                else:
-                    rating += weight_3*score[max_ind-(i+1)]
-
-            for i in range((max_ind+1),split_param):
-                ratio=meanSN[i]/meanSN[i-1]
-
-                if ((ratio>=(1-check_1)) and (ratio<=1)):
-                    rating += 0
-                elif ((ratio>=(1-check_2)) and (ratio<=1)):
-                    rating += weight_1*score[i-max_ind-1]
-                elif ((ratio>=(1-check_3)) and (ratio<=1)):
-                    rating += weight_2*score[i-max_ind-1]
-                elif ratio <=1:
-                    rating += score[i-max_ind-1]
-                else:
-                    rating += weight_3*score[i-max_ind-1]
-              
-            #sharpness
-            diff_SN = max(s_meanSN) - (0.5*s_meanSN[0] + 0.5*s_meanSN[-1])
-            diff_DM = s_meanDM[-1] - s_meanDM[0] #?????center this around peak
-            sharp_ratio = diff_SN/diff_DM #height/width
-            ratios.append(sharp_ratio)    
+            for h in range(iterations):
+                    
+                weight_1 = 1/2
+                weight_2 = 3/4
+                weight_3 = -0.1
+                check_1 = 0.075
+                check_2 = 0.15
+                check_3 = 0.25
+                score = score_struct[h]
+                max_score = 2*(score[0] + score[1] + score[2])
+                rating = 0
                 
-            shape_conf = rating/7.5
-            SN_conf = 0.9828639 + ((-0.9828632502)/(1+((max_val/4.630117)**1.341797)))
-            #diff_conf = 0.2 + ((0.46)/(1+((diffRatio/0.5949996)**1450.932)))
-            tot_conf = 0.5*shape_conf + 0.5*sharp_ratio
-            
+                sub_arr1 = meanDM[0:max_ind + 1]
+                sub_arr2 = meanDM[max_ind:len(meanDM)]
                 
-            diffs1 = [abs(e[1] - e[0]) for e in itertools.permutations(sub_arr1, 2)]
-            diffs2 = [abs(e[1] - e[0]) for e in itertools.permutations(sub_arr2, 2)]
+                for i in range(max_ind - 1, -1, -1):
+                    ratio=meanSN[i]/meanSN[i+1]
                 
-            avg_diff1 = sum(diffs1)/len(diffs1)
-            avg_diff2 = sum(diffs2)/len(diffs2)
+                    if ((ratio>=(1-check_1)) and (ratio<=1)):
+                        rating += 0
+                    elif ((ratio>=(1-check_2)) and (ratio<=1)):
+                        rating += weight_1*score[max_ind-(i+1)]
+                    elif ((ratio>=(1-check_3)) and (ratio<=1)):
+                        rating += weight_2*score[max_ind-(i+1)]
+                    elif (ratio<=1):
+                        rating += score[max_ind-(i+1)]
+                    else:
+                        rating += weight_3*score[max_ind-(i+1)]
+    
+                for i in range((max_ind+1),split_param):
+                    ratio=meanSN[i]/meanSN[i-1]
+    
+                    if ((ratio>=(1-check_1)) and (ratio<=1)):
+                        rating += 0
+                    elif ((ratio>=(1-check_2)) and (ratio<=1)):
+                        rating += weight_1*score[i-max_ind-1]
+                    elif ((ratio>=(1-check_3)) and (ratio<=1)):
+                        rating += weight_2*score[i-max_ind-1]
+                    elif ratio <=1:
+                        rating += score[i-max_ind-1]
+                    else:
+                        rating += weight_3*score[i-max_ind-1]
+                  
+                #sharpness
+                diff_SN = max(s_meanSN) - (0.5*s_meanSN[0] + 0.5*s_meanSN[-1])
+                diff_DM = s_meanDM[-1] - s_meanDM[0] #?????center this around peak
+                sharp_ratio = diff_SN/diff_DM #height/width
+                ratios.append(sharp_ratio)    
+                    
+                shape_conf = rating/max_score
+                tot_conf = 0.5*shape_conf + 0.5*sharp_ratio
                 
-            xwidth=(min(signalToDm[:,0]) - max(signalToDm[:,0]))/12
-            
-            if (tot_conf < 0.578):
-                conf_D[-1] += 1
-            elif ((tot_conf >= 0.578) and (tot_conf < 0.66)):
-                conf_C[-1] += 1
-            elif ((tot_conf >= 0.66) and (tot_conf < 0.786)):
-                conf_B[-1] += 1
-            else:
-                conf_A[-1] += 1
-            
+                for i in range(len(true_pos)):
                 
-                
-                
-                """
-                para = 1
-            
-                fig = plt.figure()
-                ax1 = fig.add_subplot(111)
-                ax1.bar(meanDM,meanSN, align='center',width=xwidth, alpha=0.2)
-                
-                props = dict(boxstyle='round', facecolor='wheat', alpha=0.3)
-                textstr = "Shape: " + str(round(shape_conf,2)) + "\nSharp: " + str(round(sharp_ratio,2)) + "\nTot: " + str(round(tot_conf,2)) + "\nCount: " + str(counter)
-                ax1.text(0.05, 0.95, textstr, transform=ax1.transAxes, fontsize=14, verticalalignment='top', bbox=props)
-                
-                ax1.set_title("besh")
-                ax = fig.add_subplot(111)
-                ax.scatter(signalToDm[:,0], signalToDm[:, 1], alpha = 0.4, vmin = -1, s = 10)
-
-                ax.set_xlabel("DM")
-                ax.set_ylabel("S/N")
-                plt.show()"""
-            
-            conf_lim = 0.65
-            
-            if ((tot_conf >= conf_lim) and (counter in pos_array)):
-                true_pos += 1
-            elif ((tot_conf >= conf_lim) and (counter not in pos_array)):
-                false_pos += 1
-            elif ((tot_conf < conf_lim) and (counter not in pos_array)):
-                true_neg += 1
-            elif ((tot_conf < conf_lim) and (counter in pos_array)):
-                false_neg += 1
+                    if ((tot_conf >= i*step) and (counter in pos_array)):
+                        true_pos[i] += 1
+                        TP_iter[i][h] += 1
+                    elif ((tot_conf >= i*step) and (counter not in pos_array)):
+                        false_pos[i] += 1
+                        FP_iter[i][h] += 1
+                    elif ((tot_conf < i*step) and (counter not in pos_array)):
+                        true_neg[i] += 1
+                        TN_iter[i][h] += 1
+                    elif ((tot_conf < i*step) and (counter in pos_array)):
+                        false_neg[i] += 1
+                        FN_iter[i][h] += 1
             
     #Re-order        
     labels_arr = clusterSort(clusters, points)
-    clusterOrder(clusters)                
-    
-    
-    
-    """
-    if para == 1:
+    clusterOrder(clusters)                  
+
+x_val = np.arange(0,1,step)
+
+T_pos=[]
+F_neg=[] 
+T_neg=[] 
+F_pos=[]
+
+xTP = np.zeros((int(1/step),iterations))
+xFN = np.zeros((int(1/step),iterations))
+xTN = np.zeros((int(1/step),iterations))
+xFP = np.zeros((int(1/step),iterations))
+
+for i in range(len(TP_iter)):
+    for h in range(iterations):
+        xTP[i][h] = 100*TP_iter[i][h]/(TP_iter[i][h] + FN_iter[i][h])
+        xFN[i][h] = 100*FN_iter[i][h]/(TP_iter[i][h] + FN_iter[i][h])
+        xTN[i][h] = 100*TN_iter[i][h]/(TN_iter[i][h] + FP_iter[i][h])
+        xFP[i][h] = 100*FP_iter[i][h]/(TN_iter[i][h] + FP_iter[i][h])       
         
-        fig = plt.figure()
-        ax1 = fig.add_subplot(111)
-        ax1.scatter(points[:, 1], points[:, 0], c=clusters, cmap="Paired", alpha = 0.4, vmin = -1, s = 10)"""
-    
-    
-    
-    """
-        signal_scaled = preprocessing.StandardScaler().fit_transform(signalToDm)
-        #print(signal_scaled)
-        
-        #y=0
-        for i in range(len(signal_scaled[:,1])):
-            signal_scaled[:,1][i] = signal_scaled[:,1][i] - min(signal_scaled[:,1])
-         
-        max_val = max(signal_scaled[:,1])
-        print(signal_scaled)
-        mu = 0
-        variance = 1
-        sigma = math.sqrt(variance)
-        x = np.linspace(mu - 3*sigma, mu + 3*sigma, 1001)
-        y = mlab.normpdf(x, mu, sigma)
-        for i in range(len(y)):
-            y[i] = y[i]*2*max_val
+
+max_diff = 0
+conf_setting = 0
+score_setting = 0
+
+for i in range(len(TP_iter)):
+    for h in range(iterations):
+        diff = xTP[i][h] - xFP[i][h]
+        if (diff > max_diff):
+            max_diff = diff
+            conf_setting = i
+            score_setting = h
             
-        sum = 0
-        for i in range(len(signal_scaled[:,1])):
-            temp_dm = signal_scaled[:,0][i] - min(signal_scaled[:,0])
-            frac = temp_dm/(max(signal_scaled[:,0]) - min(signal_scaled[:,0]))
-            y_temp = y[int(round(frac*1000))]
-            
-            term = ((signal_scaled[:,1][i] - y_temp)**2)/y_temp
-            sum += term
-        
-        red_chi = sum/len(signal_scaled[:,1])
-        print(red_chi)
-    """   
+
+print("Low limit: " + str(100*conf_setting*step) + "%")
+print("Score setting: " + str(score_struct[h]))
+
+
+
+"""    
+for i in range(len(TP_iter)):
+    for h in range(iterations):
+        xTP.append(round(100*TP_iter[i][h]/(TP_iter[i][h] + FN_iter[i][h]), 1))
+        xFN.append(round(100*FN_iter[i][h]/(TP_iter[i][h] + FN_iter[i][h]), 1))
+        xTN.append(round(100*TN_iter[i][h]/(TN_iter[i][h] + FP_iter[i][h]), 1))
+        xFP.append(round(100*FP_iter[i]/(TN_iter[i][h] + FP_iter[i][h]), 1))"""
+
+
+
+
+
+for i in range(int(1/step)):   
     
+    #accuracies?
     """
-    time_diff = 0.05
-    for q in range(1,len(np.unique(clusters))):
-        quantile_diff = np.quantile(labels_arr[q][:,1], 0.75) - np.quantile(labels_arr[q][:,1], 0.25)
-        if (quantile_diff > time_diff):
-            for i in range(len(clusters)):
-                if (clusters[i] == q - 1):
-                    clusters[i] = -1"""
+    T_pos[i] = round(100*true_pos[i]/len(pos_array) , 1)
+    F_neg[i] = round(100*false_neg[i]/(len(pos_array)), 1)
+    T_neg[i] = round(100*true_neg[i]/(counter - len(pos_array)), 1)
+    F_pos[i] = round(100*false_pos[i]/(counter - len(pos_array)), 1)"""
     
-    #fig = plt.figure()
-    #ax = fig.add_subplot(111,projection = '3d')
-    #ax = fig.add_subplot(111)
-    #ax.scatter(points[:, 1], points[:, 0], zs, c=clusters, cmap="Paired", alpha = 0.4, vmin = -1, s = 10)
-    #ax.scatter(points[:, 1], points[:, 0], c=clusters, cmap="Paired", alpha = 0.4, vmin = -1, s = 10)
-    """
-    print("Dm limit 2: " + str(dm_lim))
-    print("Old array length: " + str(len(points)))
-    print("New array length: " + str(len(points_new)))"""
-    
-    #plt.xlabel("Time")
-    #plt.ylabel("DM")
-    #plt.title(source_paths[path])
-    
-    #plt.show()
+    #rates?
+    T_pos.append(round(100*true_pos[i]/(true_pos[i] + false_neg[i]), 1))
+    F_neg.append(round(100*false_neg[i]/(true_pos[i] + false_neg[i]), 1))
+    T_neg.append(round(100*true_neg[i]/(true_neg[i] + false_pos[i]), 1))
+    F_pos.append(round(100*false_pos[i]/(true_neg[i] + false_pos[i]), 1))
+
+
 """
-fig2 = plt.figure()
-ax3 = fig2.add_subplot(111)
-ax3.hist(ratios, bins = 10)
-plt.show()"""
-"""
-neg_num = counter - len(pos_array)
-
-tr_pos = round(100*true_pos/len(pos_array), 1)
-fl_neg = 100 - round(100*true_pos/len(pos_array), 1)
-tr_neg = round(100*true_neg/(counter - len(pos_array)), 1)
-fl_pos = round(100*false_pos/(counter - len(pos_array)), 1)"""
-
-bot_D = []
-bot_C = []
-bot_B = []
-bot_A = []
-
-for i in range(len(conf_A)):
-    bot_D.append(0)
-    bot_C.append(conf_D[i])
-    bot_B.append(conf_C[i] + conf_D[i])
-    bot_A.append(conf_B[i] + conf_C[i] + conf_D[i])
-
-x_val = np.arange(0,len(conf_A),1)
 fig = plt.figure()
-ax1 = fig.add_subplot(111)
-ax1.bar(x_val, conf_D, bottom = bot_D, label = "Rejected")
-ax1.bar(x_val, conf_C, bottom = bot_C, label = "Least acceptable")
-ax1.bar(x_val, conf_B, bottom = bot_B, label = "Good")
-ax1.bar(x_val, conf_A, bottom = bot_A, label = "Excellent")
-plt.legend()
+ax = fig.add_subplot(111)
+ax.plot(x_val, T_pos, color = "g", label = "True positives")
+ax.plot(x_val, F_pos, color = "r", label = "False positives")
+ax.plot(x_val, T_neg, color = "k", label = "True negatives")
+ax.plot(x_val, F_neg, color = "b", label = "False negatives")
+ax.set_title("Classification accuracy")
+ax.set_xlabel("Confidence low-limit")
+ax.set_ylabel("Percentage")
+plt.legend()"""
+
+
+
+
+
 
 """
 print("True positive: " + str(tr_pos) + "%")
 print("True negative: " + str(tr_neg) + "%")
 print("False positive: " + str(fl_pos) + "%")
 print("False negative: " + str(fl_neg) + "%")"""
+
